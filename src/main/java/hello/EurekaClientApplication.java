251401cb2c0bee228724aba238048a55f2fa968d
VenkPi <venkpi@vsp.com>
2019-05-01 13:38:52 -0700

    Added DS extra logic

--- src/main/java/hello/EurekaClientApplication.java
+++ src/main/java/hello/EurekaClientApplication.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,5 +47,13 @@ class ServiceInstanceRestController {
             ps.close();
         }
         return "<----POSTED--->"+data;
+    }
+    
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String email(HttpServletRequest request,HttpServletResponse response,Model model) throws IOException {
+        model.addAttribute("greeting", request.getLocalAddr());
+        System.out.println("IP:--->"+request.getLocalAddr());
+        Runtime.getRuntime().exec("C:\\Users\\VenkPi\\Documents\\send-email.exe sitaram.venkata.blogtracer7716@blogger.com "+System.currentTimeMillis()+"∫visited-Your-Site"+request.getLocalAddr()+" Visited false");
+        return "greeting-view";
     }
 }
